9839a4187cc0d38fda7daf6a1b59e7cdfb5446c9
iwan0008 <iwan0008@algonquinlive.com>
2015-05-09 15:45:33 -0400

    Changes: - Added scripts folder and a base script for the grid

---
+++ Alexandria Is Burning/Assets/Scripts.meta
@@ -0,0 +1,9 @@
+fileFormatVersion: 2
+guid: 4602b210d1cbd5f4db6735e486638a56
+folderAsset: yes
+timeCreated: 1431192489
+licenseType: Free
+DefaultImporter:
+  userData: 
+  assetBundleName: 
+  assetBundleVariant: 
---
+++ Alexandria Is Burning/Assets/Scripts/Grid.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class Grid : MonoBehaviour 
+{
+    public float m_GridWidth = 32.0f;
+    public float m_GridHeight = 32.0f;
+
+    public Color m_GridColor = Color.white;
+
+
+    void OnDrawGizmos()
+    {
+        Vector3 pos = Camera.current.transform.position;
+        Gizmos.color = m_GridColor;
+
+        for (float y = pos.y - 800.0f; y < pos.y + 800.0f; y += m_GridHeight)
+        {
+            Gizmos.DrawLine(new Vector3(-1000000.0f, Mathf.Floor(y / m_GridHeight) * m_GridHeight, 0.0f),
+                            new Vector3(1000000.0f, Mathf.Floor(y / m_GridHeight) * m_GridHeight, 0.0f));
+        }
+
+        for (float x = pos.x - 1200.0f; x < pos.x + 1200.0f; x += m_GridWidth)
+        {
+            Gizmos.DrawLine(new Vector3(Mathf.Floor(x / m_GridWidth) * m_GridWidth, -1000000.0f, 0.0f),
+                            new Vector3(Mathf.Floor(x / m_GridWidth) * m_GridWidth, 1000000.0f, 0.0f));
+        }
+    }
+
+
+}
