fa3880f98e6a80363be697a995dafa0ecae164f1
iwan0008 <iwan0008@algonquinlive.com>
2015-05-17 00:49:26 -0400

    Changes: - Modified the bookcase sprite sheet - Added new bookcase prefabs - Added a level manager script (WIP)

---
+++ Alexandria Is Burning/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour 
+{
+    public static LevelManager s_Instance = null;
+    public static LevelManager instance
+    {
+        get { return s_Instance; }
+    }
+
+    public int m_CurrentLevel = 0;
+
+    [SerializeField]
+    private int m_FiresPerBookcase = 0;
+
+    [SerializeField]
+    private int m_NumberOfBookcases = 0;
+
+    void Awake()
+    {
+        if (s_Instance == null)
+        {
+            s_Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    void AdvanceToNextLevel()
+    {
+
+    }
+}
